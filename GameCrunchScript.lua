db1076a427e816977d2204806bc71863c8b59469
Game-Crunch <116602911+Game-Crunch@users.noreply.github.com>
2023-01-09 01:17:45 -0500

    Update GameCrunchScript.lua

--- GameCrunchScript.lua
+++ GameCrunchScript.lua
@@ -22,9 +22,9 @@ end
 if auto_updater == true then error("Invalid auto-updater lib. Please delete your Stand/Lua Scripts/lib/auto-updater.lua and try again") end
 
 auto_updater.run_auto_update({
-    source_url="https://github.com/Game-Crunch/GameCrunchScript/blob/main/GameCrunchScript.lua",
+    source_url="https://raw.githubusercontent.com/Game-Crunch/GameCrunchScript/main/GameCrunchScript.lua",
     script_relpath=SCRIPT_RELPATH,
-    verify_file_begins_with="GameCrunch"
+    verify_file_begins_with=""
 })
 
 -- End of auto-updater
